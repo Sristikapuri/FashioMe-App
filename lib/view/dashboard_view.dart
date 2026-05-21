f5f37e7ed30d3e39960bae5891ee86609f7b0e65
Sristika Puri <Sristikapuri2057@gmail.com>
2026-05-21 17:40:32 +0545

    Add dashboard app bar

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -8,7 +8,57 @@ class DashboardView extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffF8F5F2),
 
-      body: SafeArea(
+      // ================= APP BAR =================
+      appBar: AppBar(
+        backgroundColor: const Color(0xffF8F5F2),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+
+        title: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+          children: [
+
+            /// MENU ICON
+            const Icon(
+              Icons.menu,
+              color: Color(0xff6B0000),
+            ),
+
+            /// LOGO
+            const Text(
+              "FashioMe",
+              style: TextStyle(
+                color: Color(0xff6B0000),
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+
+            /// PROFILE ICON
+            Container(
+              padding: const EdgeInsets.all(4),
+
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+
+                border: Border.all(
+                  color: const Color(0xff6B0000),
+                ),
+              ),
+
+              child: const Icon(
+                Icons.person_outline,
+                size: 18,
+                color: Color(0xff6B0000),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      // ================= BODY =================
+      body: const SafeArea(
         child: Center(
           child: Text(
             "Dashboard",
