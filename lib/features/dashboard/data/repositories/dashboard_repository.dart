f9805747a854b8346374c762ffd5e4802782fa5e
Sristika puri <167404204+Sristikapuri@users.noreply.github.com>
2026-06-17 11:30:37 +0545

    Update dashboard_repository.dart

--- lib/features/dashboard/data/repositories/dashboard_repository.dart
+++ lib/features/dashboard/data/repositories/dashboard_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:fashio_me/core/error/failures.dart';
 import 'package:fashio_me/features/dashboard/data/datasources/dashboard_datasource.dart';
-import 'package:fashio_me/features/dashboard/data/datasources/local/dashboard_local_datasource.dart';
+import 'package:fashio_me/features/dashboard/dataa/datasources/local/dashboard_local_datasource.dart';
 import 'package:fashio_me/features/dashboard/data/datasources/remote/dashboard_remote_datasource.dart';
 import 'package:fashio_me/features/dashboard/domain/entities/dashboard_entity.dart';
 import 'package:fashio_me/features/dashboard/domain/repositories/dashboard_repository.dart';
@@ -27,7 +27,7 @@ class DashboardRepository implements IDashboardRepository {
   @override
   Future<Either<Failure, DashboardEntity>> getDashboardData() async {
     try {
-      // Try remote first, fall back to local.
+
       try {
         final dashboardModel = _remoteDataSource.getDashboardData();
         return Right(dashboardModel.toEntity());
